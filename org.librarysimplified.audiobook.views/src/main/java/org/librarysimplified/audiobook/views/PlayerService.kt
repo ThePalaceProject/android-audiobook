85811bd526a7dc26d9cefd8d9194df7689bbad18
Nuno Matos <nunommts@gmail.com>
2021-12-20 10:31:58 +0000

    Fixed controls not being correctly displayed in Android 12 lock screen

--- org.librarysimplified.audiobook.views/src/main/java/org/librarysimplified/audiobook/views/PlayerService.kt
+++ org.librarysimplified.audiobook.views/src/main/java/org/librarysimplified/audiobook/views/PlayerService.kt
@@ -13,8 +13,8 @@ import androidx.core.app.NotificationCompat
 class PlayerService : Service() {
 
   companion object {
-    private const val NOTIFICATION_CHANNEL_ID = "AudiobookCommands"
-    private const val NOTIFICATION_CHANNEL_NAME = "Audiobook commands"
+    private const val NOTIFICATION_CHANNEL_ID = "AudiobookPlayerCommands"
+    private const val NOTIFICATION_CHANNEL_NAME = "Audiobook player commands"
 
     private const val ACTION_PLAY = "org.librarysimplified.audiobook.views.action_play"
     private const val ACTION_PAUSE = "org.librarysimplified.audiobook.views.action_pause"
@@ -54,13 +54,17 @@ class PlayerService : Service() {
   private fun createNotificationChannel() {
 
     if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-      val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-      val notificationChannel = NotificationChannel(NOTIFICATION_CHANNEL_ID,
+      val notificationManager =
+        getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+      val notificationChannel = NotificationChannel(
+        NOTIFICATION_CHANNEL_ID,
         NOTIFICATION_CHANNEL_NAME,
-        NotificationManager.IMPORTANCE_LOW).apply {
-        this.lockscreenVisibility = Notification.VISIBILITY_PUBLIC
-        this.setShowBadge(false)
-      }
+        NotificationManager.IMPORTANCE_DEFAULT
+      )
+        .apply {
+          this.lockscreenVisibility = Notification.VISIBILITY_PUBLIC
+          this.setShowBadge(false)
+        }
 
       notificationManager.createNotificationChannel(notificationChannel)
     }
@@ -68,17 +72,23 @@ class PlayerService : Service() {
 
   private fun updateNotification() {
 
-    val playIntent = PendingIntent.getBroadcast(this, 0, Intent(ACTION_PLAY),
-      PendingIntent.FLAG_UPDATE_CURRENT)
-    val pauseIntent = PendingIntent.getBroadcast(this, 0, Intent(ACTION_PAUSE),
-      PendingIntent.FLAG_UPDATE_CURRENT)
+    val playIntent = PendingIntent.getBroadcast(
+      this, 0, Intent(ACTION_PLAY),
+      PendingIntent.FLAG_UPDATE_CURRENT
+    )
+    val pauseIntent = PendingIntent.getBroadcast(
+      this, 0, Intent(ACTION_PAUSE),
+      PendingIntent.FLAG_UPDATE_CURRENT
+    )
 
     val notification = NotificationCompat.Builder(this, NOTIFICATION_CHANNEL_ID)
       .setContentTitle(this.playerInfo.bookName)
       .setContentText(this.playerInfo.bookChapterName)
-      .setStyle(androidx.media.app.NotificationCompat.MediaStyle()
-        .setShowActionsInCompactView(0)
-        .setShowCancelButton(false))
+      .setStyle(
+        androidx.media.app.NotificationCompat.MediaStyle()
+          .setShowActionsInCompactView(0)
+          .setShowCancelButton(false)
+      )
       .setSmallIcon(this.playerInfo.smallIcon)
       .setLargeIcon(this.playerInfo.bookCover)
       .setVisibility(NotificationCompat.VISIBILITY_PUBLIC)
@@ -86,8 +96,9 @@ class PlayerService : Service() {
         if (this.playerInfo.isPlaying) {
           NotificationCompat.Action(R.drawable.ic_pause, "Pause", pauseIntent)
         } else {
-          NotificationCompat.Action(R.drawable.ic_play, "Play1", playIntent)
-        })
+          NotificationCompat.Action(R.drawable.ic_play, "Play", playIntent)
+        }
+      )
       .build()
 
     startForeground(1, notification)
