1067f44e813105e67a61d54b03cd10ad20c01a31
Nuno Matos <nuno.matos@toptal.com>
2022-12-01 15:10:55 +0000

    Fixed failing test

--- org.librarysimplified.audiobook.tests/src/main/java/org/librarysimplified/audiobook/tests/open_access/ExoManifestContract.kt
+++ org.librarysimplified.audiobook.tests/src/main/java/org/librarysimplified/audiobook/tests/open_access/ExoManifestContract.kt
@@ -658,7 +658,7 @@ abstract class ExoManifestContract {
 
     Assertions.assertEquals(
       manifest.readingOrder.size - numberOfParts,
-      exo.spineItems
+      exo.spineItems.size
     )
   }
 
