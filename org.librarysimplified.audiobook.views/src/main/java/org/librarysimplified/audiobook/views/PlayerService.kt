c0ceb3ef050e48073307f8e81d87dee09c918df3
Jonah Sage Dempcy <jdempcy@gmail.com>
2023-04-11 12:17:29 -0500

    Remove Log import

--- org.librarysimplified.audiobook.views/src/main/java/org/librarysimplified/audiobook/views/PlayerService.kt
+++ org.librarysimplified.audiobook.views/src/main/java/org/librarysimplified/audiobook/views/PlayerService.kt
@@ -10,7 +10,6 @@ import android.os.Build
 import android.os.IBinder
 import android.support.v4.media.MediaMetadataCompat
 import android.support.v4.media.session.MediaSessionCompat
-import android.util.Log
 import android.view.KeyEvent
 import androidx.core.app.NotificationCompat
 
