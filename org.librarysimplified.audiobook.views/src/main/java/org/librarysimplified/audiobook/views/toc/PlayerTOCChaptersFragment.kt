ed9653cc7fd158c68f76995cfdc3105a261994bc
Mark Raynsford <code@io7m.com>
2023-10-26 17:38:26 +0000

    Stop using the legacy alert dialog
    The legacy alert dialog doesn't play nicely with the Material 3
    theme.
    
    Affects: https://ebce-lyrasis.atlassian.net/browse/PP-649

--- org.librarysimplified.audiobook.views/src/main/java/org/librarysimplified/audiobook/views/toc/PlayerTOCChaptersFragment.kt
+++ org.librarysimplified.audiobook.views/src/main/java/org/librarysimplified/audiobook/views/toc/PlayerTOCChaptersFragment.kt
@@ -1,6 +1,5 @@
 package org.librarysimplified.audiobook.views.toc
 
-import android.app.AlertDialog
 import android.content.Context
 import android.content.DialogInterface
 import android.os.Bundle
@@ -11,6 +10,7 @@ import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.SimpleItemAnimator
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import org.librarysimplified.audiobook.api.PlayerAudioBookType
 import org.librarysimplified.audiobook.api.PlayerEvent
 import org.librarysimplified.audiobook.api.PlayerSpineElementDownloadStatus
@@ -143,7 +143,7 @@ class PlayerTOCChaptersFragment : Fragment(), PlayerTOCInnerFragment {
     this.log.debug("onMenuStopAllSelected")
 
     val dialog =
-      AlertDialog.Builder(this.context)
+      MaterialAlertDialogBuilder(this.requireContext())
         .setCancelable(true)
         .setMessage(R.string.audiobook_player_toc_menu_stop_all_confirm)
         .setPositiveButton(
