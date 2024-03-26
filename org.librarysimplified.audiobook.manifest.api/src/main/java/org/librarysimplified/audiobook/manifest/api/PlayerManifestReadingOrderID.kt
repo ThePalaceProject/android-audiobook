77ad6b633376aa6782d0603e6e958914750891db
Mark Raynsford <code@io7m.com>
2024-03-26 14:11:49 +0000

    Reference new spec

--- org.librarysimplified.audiobook.manifest.api/src/main/java/org/librarysimplified/audiobook/manifest/api/PlayerManifestReadingOrderID.kt
+++ org.librarysimplified.audiobook.manifest.api/src/main/java/org/librarysimplified/audiobook/manifest/api/PlayerManifestReadingOrderID.kt
@@ -2,11 +2,26 @@ package org.librarysimplified.audiobook.manifest.api
 
 import java.net.URI
 
+/**
+ * A reading order identifier.
+ *
+ * @see "https://github.com/ThePalaceProject/mobile-specs/tree/main/audiobook-reading-order-ids"
+ */
+
 data class PlayerManifestReadingOrderID(
   val text: String
 ) {
 
   companion object {
+
+    /**
+     * Create a reading order item identifier from the possibly null URI.
+     *
+     * @param index The reading order item index
+     * @param uri The URI
+     * @return An identifier
+     */
+
     fun create(
       index: Int,
       uri: URI?
@@ -15,7 +30,7 @@ data class PlayerManifestReadingOrderID(
         PlayerManifestReadingOrderID(uri.toString())
       } else {
         PlayerManifestReadingOrderID(
-          "urn:org.thepalaceproject:reading_order_item:$index"
+          "urn:org.thepalaceproject:readingOrderItem:$index"
         )
       }
     }
