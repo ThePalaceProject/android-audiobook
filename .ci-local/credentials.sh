3880ef110a65e1c71c272f89dc2b0fbd1bdf3fb7
Mark Raynsford <code@io7m.com>
2024-05-06 12:10:29 +0000

    Update credentials again.

--- .ci-local/credentials.sh
+++ .ci-local/credentials.sh
@@ -16,15 +16,27 @@ info()
 }
 
 #------------------------------------------------------------------------
-# Add the various configuration properties.
+# Add the Gradle properties to the project properties.
 #
 
 mkdir -p "${HOME}/.gradle" ||
   fatal "could not create ${HOME}/.gradle"
+
+CREDENTIALS_PATH=$(realpath ".ci/credentials") ||
+  fatal "could not resolve credentials path"
+
+ASSETS_PATH="${CREDENTIALS_PATH}/Certificates/Palace/Android"
+
+if [ ! -d "${ASSETS_PATH}" ]
+then
+  fatal "${ASSETS_PATH} does not exist, or is not a directory"
+fi
 
 cat >> "${HOME}/.gradle/gradle.properties" <<EOF
 org.thepalaceproject.lcp.enabled=true
 org.thepalaceproject.lcp.profile=prod
 org.thepalaceproject.findaway.enabled=true
 org.thepalaceproject.overdrive.enabled=true
+
+org.thepalaceproject.app.credentials.palace=${CREDENTIALS_PATH}
 EOF
